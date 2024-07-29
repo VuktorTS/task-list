7cdb36ce0c434ae8acd49a23f936011ef7c7d664
VuktorTS <sakalina222@gmail.com>
2024-07-30 00:23:37 +0300

    update Task component

--- src/Task/Task.jsx
+++ src/Task/Task.jsx
@@ -1,5 +1,35 @@
-function Task() {
-  return <div>Task</div>;
+import { useState } from "react";
+import TaskHeader from "../TaskHeader/TaskHeader";
+import { Card, CardControlButton, CardControls } from "./Task.styled";
+
+function Task({ task, editTask, deleteTask }) {
+  const [isEditable, setIsEditable] = useState(false);
+
+  return (
+    <Card>
+      <TaskHeader
+        task={task}
+        isEditable={isEditable}
+        setIsEditable={setIsEditable}
+        editTask={editTask}
+      />
+
+      <CardControls>
+        {isEditable && (
+          <li>
+            <CardControlButton onClick={() => setIsEditable(true)}>
+              Edit
+            </CardControlButton>
+          </li>
+        )}
+        <li>
+          <CardControlButton onClick={() => deleteTask(task.id)}>
+            Delete
+          </CardControlButton>
+        </li>
+      </CardControls>
+    </Card>
+  );
 }
 
 export default Task;

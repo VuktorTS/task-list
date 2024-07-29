5eeb666b5d52a3998975474d6d31e306b152f568
VuktorTS <sakalina222@gmail.com>
2024-07-30 00:41:45 +0300

    update TaskHeader component

--- src/TaskHeader/TaskHeader.jsx
+++ src/TaskHeader/TaskHeader.jsx
@@ -1,5 +1,38 @@
-function TaskHeader() {
-  return <div>TaskHeader</div>;
+import {
+  CardHeader,
+  CardTitle,
+  CardTitleForm,
+  CardTitleInput,
+  IconButton,
+} from "./TaskHeader.styled";
+
+function TaskHeader({ task, isEditable, setEditable, editTask }) {
+  const { title } = task;
+
+  const handleEditTask = (evt) => {
+    evt.preventDefault();
+    editTask(task.id, evt.target.title.value);
+    setEditable(false);
+  };
+
+  if (isEditable) {
+    return (
+      <CardHeader>
+        <CardTitleForm onSubmit={handleEditTask}>
+          <CardTitleInput as="input" defaultValue={title} name="title" />
+          <IconButton>
+            <img src="icons/check.svg" alt="Edit task" />
+          </IconButton>
+        </CardTitleForm>
+      </CardHeader>
+    );
+  }
+
+  return (
+    <CardHeader>
+      <CardTitle>This is a task</CardTitle>
+    </CardHeader>
+  );
 }
 
 export default TaskHeader;

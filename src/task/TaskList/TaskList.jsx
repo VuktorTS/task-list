f3e371eef74cea898b909b65aaad02a109899cce
VuktorTS <sakalina222@gmail.com>
2024-07-31 22:07:05 +0300

    update TaskList

--- src/task/TaskList/TaskList.jsx
+++ src/task/TaskList/TaskList.jsx
@@ -1,11 +1,18 @@
 import { Lane } from "./TaskList.styled";
 import Task from "../Task/Task";
 import TaskAdd from "../TaskAdd/TaskAdd";
+import useTask from "../hooks/useTask";
 
 function TaskList() {
+  const {
+    state: { tasks },
+  } = useTask();
+
+  const taskIds = tasks.map(({ id }) => id);
+
   return (
     <Lane>
-      {[].map((task) => (
+      {taskIds.map((task) => (
         <Task key={task.id} id={task.id} />
       ))}
       <TaskAdd />

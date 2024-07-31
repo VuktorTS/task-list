9b7f1ed7a777333b4fb597fb4b5d51aacf62bc30
VuktorTS <sakalina222@gmail.com>
2024-07-31 18:59:45 +0300

    update TaskList

--- src/task/TaskList/TaskList.jsx
+++ src/task/TaskList/TaskList.jsx
@@ -1,49 +1,14 @@
-import { useEffect, useState } from "react";
 import { Lane } from "./TaskList.styled";
-import { nanoid } from "nanoid";
 import Task from "../Task/Task";
 import TaskAdd from "../TaskAdd/TaskAdd";
 
-const initialState = [
-  { id: 1, title: "Make task manager" },
-  { id: 2, title: "Now add some more tasks" },
-];
-
-const getInitialState = () => {
-  return (
-    JSON.parse(localStorage.getItem("task-manager-item-list")) || initialState
-  );
-};
-
 function TaskList() {
-  const [tasks, setTasks] = useState(getInitialState);
-
-  useEffect(() => {
-    localStorage.setItem("task-manager-item-list", JSON.stringify(tasks));
-  }, [tasks]);
-
-  const addTask = (title) =>
-    setTasks((task) => task.concat({ id: nanoid(), title }));
-
-  const editTask = (id, title) =>
-    setTasks((task) =>
-      task.map((ts) => (ts.id === id ? { ...ts, title } : ts)),
-    );
-
-  const deleteTask = (id) =>
-    setTasks((task) => task.filter((ts) => ts.id !== id));
-
   return (
     <Lane>
-      {tasks.map((task) => (
-        <Task
-          key={task.id}
-          task={task}
-          editTask={editTask}
-          deleteTask={deleteTask}
-        />
+      {[].map((task) => (
+        <Task key={task.id} id={task.id} />
       ))}
-      <TaskAdd addTask={addTask} />
+      <TaskAdd />
     </Lane>
   );
 }

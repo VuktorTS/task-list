134614992a1aafbcc1431200e1d3ec05599e0ac0
VuktorTS <skalina222@gmail.com>
2024-07-31 19:37:34 +0300

    add ContextProvider

---
+++ src/task/TaskProvider.jsx
@@ -0,0 +1,49 @@
+import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
+import TaskContext from "./context";
+
+const initialState = [
+  { id: 1, title: "1", step: [{ step: "1", completed: true }] },
+];
+
+const getInitialState = () =>
+  JSON.parse(localStorage.getItem("task-manager-items-steps")) || initialState;
+
+function TaskProvider({ children }) {
+  const [expandedId, setExpandedId] = useState(null);
+
+  const toggleExpand = (id) =>
+    setExpandedId((oldId) => (oldId === id ? null : id));
+
+  const [tasks, setTasks] = useState(getInitialState);
+
+  useEffect(() => {
+    localStorage.setItem("task-manager-items-steps", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = (title) =>
+    setTasks((ts) => ts.concat([{ id: nanoid(), title, step: [] }]));
+
+  const editTask = (id, title) =>
+    setTasks((ts) =>
+      ts.map((task) => (task.id === id ? { ...task, title } : task)),
+    );
+  const deleteTask = (id) =>
+    setTasks((ts) => ts.filter((task) => task.id !== id));
+
+  const value = {
+    state: {
+      expandedId,
+      tasks,
+    },
+    action: {
+      toggleExpand,
+      addTask,
+      editTask,
+      deleteTask,
+    },
+  };
+  return <TaskContext.Provider value={value}>{children}</TaskContext.Provider>;
+}
+
+export default TaskProvider;

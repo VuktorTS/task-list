eb75e2185d9bcd9932cc39bc00ef02f4f0063a9e
VuktorTS <skalina222@gmail.com>
2024-08-04 23:20:34 +0300

    add marckup in StepCheckbox

--- src/step/StepCheckbox/StepCheckbox.jsx
+++ src/step/StepCheckbox/StepCheckbox.jsx
@@ -1,5 +1,27 @@
-function StepChekbox() {
-  return <div>StepChekbox</div>;
+import useTask from "../../task/hooks/useTask";
+import { StepLabel } from "./StepCheckbox.styled";
+
+function StepCheckbox({ taskId, index }) {
+  const {
+    state: { tasks },
+    actions: { editSteps },
+  } = useTask();
+
+  const stepData = tasks.find((task) => task.id === taskId).steps[index];
+
+  const handleCheck = () =>
+    editSteps(taskId, index, { completed: !stepData.completed });
+
+  return (
+    <StepLabel>
+      <input
+        type="checkbox"
+        checked={stepData.completed}
+        onChange={handleCheck}
+      />
+      {stepData.completed ? <s>{stepData.step}</s> : stepData.step}
+    </StepLabel>
+  );
 }
 
-export default StepChekbox;
+export default StepCheckbox;

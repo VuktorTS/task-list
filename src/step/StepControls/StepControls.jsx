b0139a68b4a65d46c2cba48be94f693e657f898f
VuktorTS <sakalina222@gmail.com>
2024-08-04 23:32:57 +0300

    add marckup in SttepControls

--- src/step/StepControls/StepControls.jsx
+++ src/step/StepControls/StepControls.jsx
@@ -1,5 +1,18 @@
-function StepControls() {
-  return <div>StepControls</div>;
+import Button from "../../Button/Button";
+import useTask from "../../task/hooks/useTask";
+
+function StepButton(props) {
+  return <Button className="step-button" {...props} />;
+}
+
+function StepControls({ taskId, index }) {
+  const {
+    actions: { deleteStep },
+  } = useTask();
+
+  const onClick = () => deleteStep(taskId, index);
+
+  return <StepButton icon="trash" label="Delete" onClick={onClick} />;
 }
 
 export default StepControls;

8b3273dacf933bf59191e1d06f9773bc90d2acac
VuktorTS <skalina222@gmail.com>
2024-08-04 23:55:33 +0300

    add marckup and style in StepAdd

--- src/step/StepAdd/StepAdd.jsx
+++ src/step/StepAdd/StepAdd.jsx
@@ -1,5 +1,26 @@
+import Button from "../../Button/Button";
+import useTask from "../../task/hooks/useTask";
+import { AddStep, StepForm, StepInput } from "./StepAdd.styled";
+
 function StepAdd() {
-  return <div>StepAdd</div>;
+  const {
+    actions: { addStep },
+  } = useTask();
+
+  const handleAddStep = (evt) => {
+    evt.preventDefault();
+    addStep(taskId, evt.target.step.value);
+    evt.target.reset();
+  };
+
+  return (
+    <AddStep>
+      <StepForm onSubmit={handleAddStep}>
+        <StepInput placeholder="Add new step" name="step" />
+        <Button className="step-button" icon="plus" label="Add new step" />
+      </StepForm>
+    </AddStep>
+  );
 }
 
 export default StepAdd;

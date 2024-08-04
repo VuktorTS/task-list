a9bcbe44da3e81f9ae8b1a21baaf5517a3427ab4
VuktorTS <sakalina222@gmail.com>
2024-08-04 13:05:21 +0300

    update TaskHeader

--- src/task/TaskHeader/TaskHeader.styled.js
+++ src/task/TaskHeader/TaskHeader.styled.js
@@ -11,6 +11,7 @@ export const CardHeader = styled.header`
   text-align: left;
   cursor: pointer;
 `;
+
 export const CardTitleForm = styled.form`
   flex-grow: 1;
   display: flex;
@@ -18,6 +19,7 @@ export const CardTitleForm = styled.form`
   gap: 8px;
   margin: -1px 0;
 `;
+
 export const CardTitle = styled.p`
   flex-grow: 1;
   margin: 0;
@@ -29,6 +31,7 @@ export const CardTitleInput = styled(CardTitle)`
   width: 10px;
   margin: -2px 0 -2px -4px;
 `;
+
 export const IconButton = styled.button`
   background-color: transparent;
   border: 0;
@@ -36,7 +39,15 @@ export const IconButton = styled.button`
   color: black;
   display: flex;
   cursor: pointer;
+
   & img {
     width: 16px;
+  }
+`;
+export const CardExpand = styled(IconButton)`
+  transform: rotate(-0.25turn);
+
+  & .card-expanded {
+    transform: none;
   }
 `;

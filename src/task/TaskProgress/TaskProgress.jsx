c7a9d893d733d39b34e68977c653557f4d58a23b
VuktorTS <skalina222@gmail.com>
2024-08-05 21:14:24 +0300

    add marckup and style in TaskProgress

--- src/task/TaskProgress/TaskProgress.jsx
+++ src/task/TaskProgress/TaskProgress.jsx
@@ -1,5 +1,17 @@
+import useTask from "../hooks/useTask";
+import { ProgressBar } from "./TaskProgress.styled";
+
 function TaskProgress() {
-  return <div>TaskProgress</div>;
+  const {
+    state: { tasks },
+  } = useTask();
+
+  const steps = tasks.find((task) => task.id === id).steps;
+
+  const stepsTotal = steps.length;
+  const stepsCompleted = steps.filter(({ completed }) => completed).length;
+
+  return <ProgressBar max={stepsTotal} value={stepsCompleted} />;
 }
 
 export default TaskProgress;
---
+++ src/task/TaskProgress/TaskProgress.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ProgressBar = styled.progress`
+  flex-grow: 1;
+  min-height: 7px;
+  width: calc(100% + 32px);
+  margin: 0 -16px -8px;
+`;
